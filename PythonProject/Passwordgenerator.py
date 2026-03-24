6f08b2e9f8be16f2a89af109e4f8ee856db4f8d1
h0059ebp <89545643+h0059ebp@users.noreply.github.com>
2026-03-24 07:24:38 +0100

    Delete PythonProject directory - File Cleanup

--- PythonProject/Passwordgenerator.py
+++
@@ -1,8 +0,0 @@
-import random, string
-
-passwordlentgth = int(input("Enter the length of the password: "))
-random_string = ''.join(random.choice(string.ascii_letters) for _ in range(passwordlentgth))
-print(random_string)  # e.g. 'aKdLmNpQrT'
-
-print("Hello World")
-print(23+22)
